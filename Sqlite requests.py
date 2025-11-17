b293deb6058985d6f0149140fc2042421bdd77b3
Nikerontik <nikolajanosov6106@gmail.com>
2025-11-17 22:33:02 +0300

    Удаление лишних файлов второй раз

--- Sqlite requests.py
+++ Sqlite requests.py
@@ -14,65 +14,68 @@
 # cursor.execute('SELECT id FROM test GROUP BY id HAVING AVG(id) = ?', (4,)) #group by - группировка данных по определенным столбцам
 # mid = cursor.fetchall()
 
+
 import sqlite3 #Библиотека для работы с бд
 
 # Устанавливаем соединение с бд
-connection = sqlite3.connect("test.db")
+connection = sqlite3.connect("KitovTop.db")
 cursor = connection.cursor()
 
-# Создаем таблицу Users
-#cursor.execute - Отвечает за отправку запросов
-# cursor.execute('''
-# CREATE TABLE IF NOT EXISTS Users (
-# id INTEGER PRIMARY KEY AUTOINCREMENT,
-# username TEXT NOT NULL,
-# email TEXT NOT NULL,
-# age INTEGER
-# )
-# ''')
+#Таблица "Компьютеры"
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Computer (
+id_pc INTEGER PRIMARY KEY AUTOINCREMENT,
+Email TEXT NOT NULL,
+id_proga INTEGER,
+FOREIGN KEY(id_proga) REFERENCES PO(id)
+)
+''')
 
-#Подсчитываем количество записей в
-cursor.execute('SELECT COUNT(*) FROM test')
-total_users = cursor.fetchone()[0]
 
-# Выбираем всех пользователей
-cursor.execute('SELECT * FROM test')
-users = cursor.fetchall() #Возвращает все строки результата запроса в виде списка
+cursor.execute('''
+INSERT INTO Computer (Email, id_proga) 
+VALUES('gadakoifrazu-3290@yopmail.com', '2');''')
 
-# Выбор опеределенного пользователя через SELECT и WHERE
-cursor.execute('SELECT name, phone FROM test WHERE id = ?', (3,))
-find_user = cursor.fetchall()
+cursor.execute('''
+INSERT INTO Computer (Email, id_proga) 
+VALUES('jojeuneyecau-4122@yopmail.com', '1');''')
 
-# Вывод результатов
-for name in users:
+cursor.execute('''
+INSERT INTO Computer (Email, id_proga) 
+VALUES('mebitrudimma-3668@yopmail.com', '3');''')
+
+
+
+#Таблица "ПО"
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS PO (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+id_package INTEGER,
+Proga TEXT NOT NULL,
+FOREIGN KEY(id_package) REFERENCES Computer(id_pc)
+)
+''')
+
+cursor.execute('''
+INSERT INTO PO (Proga)
+VALUES('PyCharm');''')
+
+cursor.execute('''
+INSERT INTO PO (Proga) 
+VALUES('Visual Studio');''')
+
+cursor.execute('''
+INSERT INTO PO (Proga) 
+VALUES('JetBrains Package');''')
+
+#Выводим все виде одной таблицы
+
+cursor.execute('SELECT Computer.id_pc,Computer.Email,PO.Proga FROM Computer JOIN PO ON Computer.id_proga = PO.ID') #Соединяем две колонки
+Sout = cursor.fetchall() #Возвращает все строки результата запроса в виде списка
+
+for name in Sout:
     print(name)
-print("Количество пользователей: ", total_users)
-print("Пробитый пользователь:", find_user)
 
 # Сохраняем изменения и закрываем соединение
 connection.commit()
 connection.close()
-
-'''
-///Эта я прописывал в консоли sqlite///
-CREATE TABLE test (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, #integer - целое число; primary key - поле id должно быть уникальным во всей таблице; AUTOINCREMENT - сама присваивает id, в mysql пишется с подчеркиванием
-    name VARCHAR(50), #varchar - переменная длины символьная строка (символ)
-    phone VARCHAR(50)); #varchar - переменная длины символьная строка (символ)
-#SQL слова пишу большими буквами
-
-INSERT INTO test (name, phone)
-VALUES ('Nike', '89524722056');
-
-INSERT INTO test (name, phone)
-VALUES ('George', '+79200173800');
-
-INSERT INTO test (name, phone)
-VALUES ('Mixa', '+79601707518');
-
-INSERT INTO test (name, phone)
-VALUES ('Nata', '+79883140249');
-
-INSERT INTO test (name, phone)
-VALUES ('Individum', '+79200294472');
-'''
